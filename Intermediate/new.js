8ee94c0850b4301b25f2ac3b125dd0a3241e0325
Aman Kumar <amancollege04@example.com>
2024-08-31 13:52:30 +0530

    Learned about new keyword

---
+++ Intermediate/new.js
@@ -0,0 +1,8 @@
+/*
+When we use new keyword:
+A new object is created: The new keyword initiates the creation of new JS Object.
+
+The object is initialized with the properties and methods defined in the class: The new keyword calls the constructor
+
+
+*/
