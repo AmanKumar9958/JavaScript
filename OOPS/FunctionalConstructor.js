8a8e229070bbde4ff1184201a645ba6b83e1bfcb
Aman Kumar <amancollege04@gmail.com>
2025-02-12 13:06:07 +0530

    Done: Constructor

--- OOPS/FunctionalConstructor.js
+++ OOPS/FunctionalConstructor.js
@@ -1,16 +1,59 @@
-function Animal(name, eatingBehaviour){
+// // Animal is our constructor..
+// function Animal(name, eatingBehaviour){
+//     this.name = name;
+//     this.eatingBehaviour = eatingBehaviour;
+//     this.getEatingBehaviour = function(){
+//         return `${this.name} is an ${this.eatingBehaviour}`;
+//     };
+// }
+
+// // Prototype is used to add properties to the object...
+// Animal.prototype.sound = function(soundType){
+//     return `${this.name} ${soundType}`;
+// }
+
+// // Creating an object of Animal
+// let dog = new Animal("Dog", "Omnivorous");
+// console.log(dog.getEatingBehaviour());
+// console.log(dog.sound("Woff!!"));
+
+// // creating another object of Animal - Cow
+
+// let cow = new Animal("Cow", "Herbivorous");
+// console.log(cow.getEatingBehaviour());
+// console.log(cow.sound("Mow!!"));
+
+
+// More optimized code..
+
+function Animal(name){
     this.name = name;
-    this.eatingBehaviour = eatingBehaviour;
-    this.getEatingBehaviour = function(){
-        return `${this.name} is an ${this.eatingBehaviour}`;
-    };
+    return `This is ${this.name}`;
 }
 
-let dog = new Animal("Dog", "Omnivorous");
-console.log(dog.getEatingBehaviour());
-
-
-Animal.prototype.sound = function(){
-    return `${this.name} barks`;
+Animal.prototype.sound = function(soundType){
+    return `${this.name} ${soundType}`;
 }
-console.log(dog.sound());
+
+Animal.prototype.foodType = function(foodType){
+    return `${this.name} is an ${foodType}`;
+}
+
+let dog = new Animal("Dog");
+console.log(dog.sound("Woff!!"));
+console.log(dog.foodType("Omnivorous"));
+
+let cow = new Animal("Cow");
+console.log(cow.sound("Mow!!"));
+console.log(cow.foodType("Herbivorous"));
+
+// Throwing errors..
+function car(brand){
+    if(!new.target){
+        throw new Error("Please use new keyword to create an object");
+    }
+    this.brand = brand;
+}
+
+let car1 = new car("BMW");  // no error
+let car2 = car("Ferrari");  // error
